8c36d0ff59735bf2332028176cb6e26d111a31c7
benowino7 <bensonowino7@gmail.com>
2020-03-02 15:38:11 +0300

    set the debug value to false

--- django_photo_gallery/settings.py
+++ django_photo_gallery/settings.py
@@ -69,13 +69,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_photo_gallery.wsgi.application'
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+# db_from_env = dj_database_url.config(conn_max_age=500)
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default = config('DATABASE_URL')
-    )
-}
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default = config('DATABASE_URL')
+#     )
+# }
+db_from_env = dj_database_url.config(conn_max_age=500) 
+DATABASES['default'].update(db_from_env)
 
 AUTH_PASSWORD_VALIDATORS = [
     {
