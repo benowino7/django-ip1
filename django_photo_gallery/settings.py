535262aa0dacc8816c639d9b7feef066495d3641
benowino7 <bensonowino7@gmail.com>
2020-03-02 15:26:56 +0300

    set the debug value to false

--- django_photo_gallery/settings.py
+++ django_photo_gallery/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY='benjail'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 
 ALLOWED_HOSTS = ['*']
